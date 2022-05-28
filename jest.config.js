b38ac28f329a5647dc209bf63915dfb03ce914ac
likangbo <67063670@qq.com>
2022-05-29 03:07:21 +0800

    chore: add jest  need test file rule

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
+  testRegex: '(/src/.*\\.(test|spec))\\.[tj]sx?$',
+
   // 是否显示覆盖率报告
-  collectCoverage: true,
-  // 告诉 jest 哪些文件需要经过单元测试测试
-  // collectCoverageFrom: ['src/**/*'],
-  // coverageThreshold: {
-  //   global: {
-  //     statements: 90, // 保证每个语句都执行了
-  //     functions: 90, // 保证每个函数都调用了
-  //     branches: 90, // 保证每个 if 等分支代码都执行了
-  //   },
-  // },
+  // collectCoverage: true,
 }
