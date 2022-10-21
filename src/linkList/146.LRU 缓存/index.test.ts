99ce428489cb7db20958c37c9dc49a90617845c9
likangbo <67063670@qq.com>
2022-10-22 00:42:12 +0800

    feat: update leetcode-146.LRU 缓存

--- src/linkList/146.LRU 缓存/index.test.ts
+++ src/linkList/146.LRU 缓存/index.test.ts
@@ -7,20 +7,70 @@ describe('LRUCache', () => {
     lRUCache.put(2, 2);
     const num1 = lRUCache.get(1);
     expect(num1).toBe(1)
-    
+
     lRUCache.put(3, 3);
     const num2 = lRUCache.get(2);
-    expect(num1).toBe(-1)
+    expect(num2).toBe(-1)
 
     lRUCache.put(4, 4);
     const num3 = lRUCache.get(1);
-    expect(num1).toBe(-1)
+    expect(num3).toBe(-1)
 
     const num4 = lRUCache.get(3);
-    expect(num1).toBe(3)
+    expect(num4).toBe(3)
 
     const num5 = lRUCache.get(4);
-    expect(num1).toBe(4)
+    expect(num5).toBe(4)
 
+  });
+  it('is ok1', () => {
+    const lRUCache = new LRUCache(2)
+    lRUCache.put(1, 0);
+    lRUCache.put(2, 2);
+    const num1 = lRUCache.get(1);
+    expect(num1).toBe(0)
+
+    lRUCache.put(3, 3);
+    const num2 = lRUCache.get(2);
+    expect(num2).toBe(-1)
+
+    lRUCache.put(4, 4);
+    const num3 = lRUCache.get(1);
+    expect(num3).toBe(-1)
+
+    const num4 = lRUCache.get(3);
+    expect(num4).toBe(3)
+
+    const num5 = lRUCache.get(4);
+    expect(num5).toBe(4)
+
+  });
+  it('is ok2', () => {
+    const lRUCache = new LRUCache(2)
+    const num1 = lRUCache.get(2);
+    expect(num1).toBe(-1)
+
+    lRUCache.put(2, 6);
+    const num2 = lRUCache.get(1);
+    expect(num2).toBe(-1)
+
+    lRUCache.put(1, 5);
+    lRUCache.put(1, 2);
+    const num3 = lRUCache.get(1);
+    expect(num3).toBe(2)
+
+    const num4 = lRUCache.get(2);
+    expect(num4).toBe(6)
+  });
+  it('is ok3', () => {
+    const lRUCache = new LRUCache(2)
+    lRUCache.put(2, 1);
+    lRUCache.put(1, 1);
+    lRUCache.put(2, 3);
+    lRUCache.put(4, 1);
+    const num1 = lRUCache.get(1);
+    expect(num1).toBe(-1)
+    const num2 = lRUCache.get(2);
+    expect(num2).toBe(3)
   });
 });
--- src/linkList/146.LRU 缓存/index.ts
+++ src/linkList/146.LRU 缓存/index.ts
@@ -1,23 +1,37 @@
-
-interface IData {
-  [key: number]: number,
-}
 export class LRUCache {
-  capacity: number = 0
-  data: Array<IData> = []
+  capacity: number;
+  map: Map<number, number> = new Map();
   constructor(capacity: number) {
     this.capacity = capacity
   }
 
   get(key: number): number {
-
-  }
-
-  put(key: number, value: number): void {
-    const obj = { key: value }
-    if (this.data.length >= this.capacity) {
-      this.data.pop()
+    const data = this.map;
+    if (!data.has(key)) {
+      return -1;
     }
-    this.data.push(obj)
+
+    const value = data.get(key) ?? -1;
+    // 返回数据前,先删除原数据,然后在添加,就可以保持在最新
+    data.delete(key);
+    data.set(key, value);
+
+    return value;
+  }
+  put(key: number, value: number): void {
+    const data = this.map;
+
+    // 如果存在该对象,则直接删除
+    if (data.has(key)) {
+      data.delete(key);
+    }
+    // 将数据对象添加到map中
+    data.set(key, value);
+
+    if (data.size > this.capacity) {
+      // 如果map长度超过最大值,则取出map中的第一个元素,然后删除
+      const delKey = data.keys().next().value
+      data.delete(delKey);
+    }
   }
 }
