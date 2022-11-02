3401baba74c9c8304e87bd6e645c31b53af0f142
likangbo <67063670@qq.com>
2022-11-02 23:57:31 +0800

    feat: update leetcode-146.LRU 缓存

--- src/linkList/146.LRU 缓存/index.ts
+++ src/linkList/146.LRU 缓存/index.ts
@@ -1,4 +1,5 @@
-export class LRUCache {
+// case1
+export class LRUCache1 {
   capacity: number;
   map: Map<number, number> = new Map();
   constructor(capacity: number) {
@@ -33,5 +34,92 @@ export class LRUCache {
       const delKey = data.keys().next().value
       data.delete(delKey);
     }
+  }
+}
+
+
+// case2
+class ListNode {
+  next: ListNode | null;
+  prev: ListNode | null;
+  key: string | number;
+  value: string | number;
+  constructor(key?: string | number, value?: any) {
+    this.key = key!
+    this.value = value
+    this.next = null
+    this.prev = null
+  }
+}
+export class LRUCache {
+  capacity: number;
+  count: number;
+  dummyHead: ListNode;
+  dummyTail: ListNode;
+  hash: any;
+  constructor(capacity: number) {
+    this.capacity = capacity
+    this.hash = {}
+    this.count = 0
+    this.dummyHead = new ListNode()
+    this.dummyTail = new ListNode()
+    this.dummyHead.next = this.dummyTail
+    this.dummyTail.prev = this.dummyHead
+  }
+
+  get(key: string | number) {
+    let node = this.hash[key]
+    if (node == null) return -1
+    this.moveToHead(node)
+    return node.value
+  }
+
+  put(key: string | number, value: any) {
+    let node = this.hash[key]
+    if (node == null) {
+      if (this.count == this.capacity) {
+        this.removeLRUItem()
+      }
+      let newNode = new ListNode(key, value)
+      this.hash[key] = newNode
+      this.addToHead(newNode)
+      this.count++
+    } else {
+      node.value = value
+      this.moveToHead(node)
+    }
+  }
+
+  moveToHead(node: ListNode) {
+    this.removeFromList(node)
+    this.addToHead(node)
+  }
+
+  removeFromList(node: ListNode) {
+    let temp1 = node.prev!
+    let temp2 = node.next!
+    temp1.next = temp2
+    temp2.prev = temp1
+  }
+
+  addToHead(node: ListNode) {
+    node.prev = this.dummyHead
+    node.next = this.dummyHead.next
+    if (this.dummyHead.next) {
+      this.dummyHead.next.prev = node
+    }
+    this.dummyHead.next = node
+  }
+
+  removeLRUItem() {
+    let tail = this.popTail()
+    delete this.hash[tail.key]
+    this.count--
+  }
+
+  popTail() {
+    let tail = this.dummyTail.prev
+    this.removeFromList(tail!)
+    return tail!
   }
 }
